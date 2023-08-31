7bf495cfb37b360c579d0f64c18a2b574de8ee0b
Olga <ozhuk30@gmail.com>
2023-08-31 22:24:40 +0300

    fix:code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let data = String(n);
+  let data2 = data.split("").reverse().join("");
+  let data3 = data2.replace(/[^a-zа-яё0-9\s]/gi, ' ');
+  let result = Number(data3);
+  return result;
 }
